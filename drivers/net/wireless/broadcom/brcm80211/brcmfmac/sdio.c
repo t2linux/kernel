ae72c664a0686326c97b2cc3ea45fd9793514d2e
Hector Martin <marcan@marcan.st>
2021-12-21 17:38:37 +0900

    brcmfmac: firmware: Support passing in multiple board_types
    In order to make use of the multiple alt_path functionality, change
    board_type to an array. Bus drivers can pass in a NULL-terminated list
    of board type strings to try for the firmware fetch.
    
    Signed-off-by: Hector Martin <marcan@marcan.st>
    ---
    Adjusted for rebase on:
    - c4caf72 ("brcmfmac: firmware: Fix crash in brcm_alt_fw_path")
    Signed-off-by: Aun-Ali Zaidi <admin@kodeit.net>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/sdio.c
@@ -4432,7 +4432,9 @@ brcmf_sdio_prepare_fw_request(struct brcmf_sdio *bus)
 	fwreq->items[BRCMF_SDIO_FW_NVRAM].type = BRCMF_FW_TYPE_NVRAM;
 	fwreq->items[BRCMF_SDIO_FW_CLM].type = BRCMF_FW_TYPE_BINARY;
 	fwreq->items[BRCMF_SDIO_FW_CLM].flags = BRCMF_FW_REQF_OPTIONAL;
-	fwreq->board_type = bus->sdiodev->settings->board_type;
+	fwreq->board_types = devm_kzalloc(bus->sdiodev->dev,
+					  sizeof(const char *) * 2, GFP_KERNEL);
+	fwreq->board_types[0] = bus->sdiodev->settings->board_type;
 
 	return fwreq;
 }
